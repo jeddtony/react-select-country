c77d51dd5044b773cf884403065038e57f139f8e
jeddtony <jedtony.jt@gmail.com>
2020-05-07 21:04:28 +0100

    Write tests for states

--- src/services/tests/controller.test.js
+++ src/services/tests/controller.test.js
@@ -4,3 +4,7 @@ test('it returns a list of countries as an array ', () => {
     let result = getAllCountries();
     expect(result).toBeInstanceOf(Array);
 })
+
+// test('It returns a list of states as an array', () => {
+    
+// })
